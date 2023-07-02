72c4aa2d6ae61036faf40cbd8f6b4fe8f7af3092
Danyil Derkach <64920477+Gnomepman@users.noreply.github.com>
2023-07-02 19:09:48 +0300

    feat: add get and post requests

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { getDatabase, saveDatabase } from './database/index';
+import http from 'http';
+import { User } from 'types';
+import url from 'url';
+
+const users: User[] = getDatabase();
+
+const sendResponse = (
+  res: http.ServerResponse,
+  statusCode: number,
+  data: unknown,
+) => {
+  res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(data));
+};
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+
+  const { pathname } = url.parse(req.url || '', true);
+
+  if (pathname === '/api/users' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  }
+  if (pathname === '/api/users' && req.method === 'POST') {
+    req.on('data', (data) => {
+      const userRequest = JSON.parse(data);
+
+      if (!userRequest.username || !userRequest.age || !userRequest.hobbies) {
+        sendResponse(res, 400, 'Required fields are missing');
+        return;
+      }
+
+      const user: User = {
+        id: users.length + 1,
+        username: String(userRequest.username),
+        age: Number(userRequest.age),
+        hobbies: Array.from(userRequest.hobbies),
+      };
+
+      users.push(user);
+      saveDatabase(users);
+      sendResponse(res, 201, user);
+    });
+  }
+});
+
+server.listen(8000); // move to .env
+
+// and test - monday evening
