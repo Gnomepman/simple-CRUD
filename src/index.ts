453bdc7c9ffcb8d40702ca35a44a60b56d655c5b
Danyil Derkach <64920477+Gnomepman@users.noreply.github.com>
2023-07-02 19:53:43 +0300

    feat: add get user by id

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { getDatabase, saveDatabase } from './database/index';
 import http from 'http';
 import { User } from 'types';
 import url from 'url';
+import querystring from 'querystring';
 
 const users: User[] = getDatabase();
 
@@ -19,11 +20,25 @@ const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'content-type');
 
-  const { pathname } = url.parse(req.url || '', true);
+  const { pathname, search } = url.parse(req.url || '', true);
 
   if (pathname === '/api/users' && req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(users));
+    if (search) {
+      const [, query] = search.split('?');
+      const data = querystring.parse(String(query));
+
+      if (!Number(data.id)) {
+        sendResponse(res, 400, 'Invalid param type. Must be number');
+        return;
+      }
+
+      const user = users.find((user) => user.id === Number(data.id));
+      user
+        ? sendResponse(res, 200, user)
+        : sendResponse(res, 404, 'User not found');
+    } else {
+      sendResponse(res, 200, users);
+    }
   }
   if (pathname === '/api/users' && req.method === 'POST') {
     req.on('data', (data) => {
